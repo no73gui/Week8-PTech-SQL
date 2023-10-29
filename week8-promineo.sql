a41f50050c7f3a30b3bfbb10bd86b04a1440cc65
no73gui <no73gui+github@gmail.com>
2023-10-28 23:13:42 -0500

    Second push, drop the rest of the tables, first : tables with foreign key ref. ; second set : referenced tables.

--- week8-promineo.sql
+++ week8-promineo.sql
@@ -1,7 +1,10 @@
 -- Drop tables with foreign key references. These tables need to be created AFTER the tables with 0 foreign key constraints.
+-- project_category has foreign keys
 DROP TABLE IF EXISTS project_category;
 DROP TABLE IF EXISTS material;
 DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS category;
 -- Create tables without foreign key constraints
 CREATE TABLE IF NOT EXISTS projects (
     project_id INT NOT NULL AUTO_INCREMENT,
@@ -12,14 +15,18 @@ CREATE TABLE IF NOT EXISTS projects (
     notes TEXT,
     PRIMARY KEY (project_id)
 );
-
 CREATE TABLE IF NOT EXISTS category (
     category_id INT NOT NULL AUTO_INCREMENT,
     category_name VARCHAR(128),
     PRIMARY KEY (category_id)
 );
-
 -- Create tables with foreign key constraints. These tables need to form relationships with pre-existing tables the server already knows about.
+CREATE TABLE IF NOT EXISTS project_category (
+    project_id INT NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE
+);
 CREATE TABLE IF NOT EXISTS material (
     material_id INT NOT NULL AUTO_INCREMENT,
     project_id INT NOT NULL,
@@ -29,7 +36,6 @@ CREATE TABLE IF NOT EXISTS material (
     PRIMARY KEY (material_id),
     FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE
 );
-
 CREATE TABLE IF NOT EXISTS step (
     step_id INT NOT NULL AUTO_INCREMENT,
     project_id INT NOT NULL,
@@ -38,11 +44,3 @@ CREATE TABLE IF NOT EXISTS step (
     PRIMARY KEY (step_id),
     FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE
 );
-
-CREATE TABLE IF NOT EXISTS project_category (
-    project_id INT NOT NULL,
-    category_id INT NOT NULL,
-    FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES category(category_id)
-);
-
